b2d793371067880228726c5d6d70f9455ca91aaf
Lou1211 <cindy20021211@gmail.com>
2021-10-13 16:37:16 +0800

    101

--- github/1013/1013-1.c
+++ github/1013/1013-1.c
@@ -3,7 +3,10 @@
 int main(void){
     int n,m;
     scanf("%d%d",&n,&m);
-    for (int i=n+n%2;i<=m;i+=2)
+    if (n%2==0){
+        n=n+1;
+    }
+    for (int i=n;i<=m;i+=2)
     {
         printf("%d\n",i);
     }
