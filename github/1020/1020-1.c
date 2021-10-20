97e8811d63c929a823fe6d097aa412a814d233d0
Lou1211 <cindy20021211@gmail.com>
2021-10-20 15:52:55 +0800

    10

---
+++ github/1020/1020-1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void){
+    int a,b,c,e;
+    scanf("%d%d",&a,&b);
+    printf("%d\n",a222(a,b));
+}
+int a222(int a,int e){
+    int b=1,c=1,d=1;
+    for (int i=1; i<=a;i++)
+    {
+        b=b*i;
+    }
+    for (int i=1; i<=e;i++)
+    {
+        c=c*i;
+    }
+    for (int i=1; i<=a-e;i++)
+    {
+        d=d*i;
+    }
+    e=(b/c)/d;
+}
