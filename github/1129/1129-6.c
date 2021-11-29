070fd0ce19629eb5af570900bb2551da60695c5e
Lou1211 <cindy20021211@gmail.com>
2021-11-29 22:51:37 +0800

    1112

--- github/1129/1129-6.c
+++ github/1129/1129-6.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int e,s=0;
-    int arr1[3][3],arr2[3][3];
-    for (int i = 0; i < 3 ; i++)
+int main()
+{
+    int e, s = 0;
+    int arr1[3][3], arr2[3][3];
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3 ; j++)
+        for (int j = 0; j < 3; j++)
         {
-            scanf("%d",&arr1[i][j]);
+            scanf("%d", &arr1[i][j]);
         }
     }
-    for (int i = 0; i < 3 ; i++)
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3 ; j++)
+        for (int j = 0; j < 3; j++)
         {
-            scanf("%d",&arr2[i][j]);
+            scanf("%d", &arr2[i][j]);
         }
     }
     for (int i = 0; i < 3; i++)
@@ -23,14 +24,14 @@ int main(){
         {
             for (int x = 0; x < 3; x++)
             {
-                int *aPrt=&arr1[i][x];
-                int *bPrt=&arr2[x][j];
-                e=*aPrt**bPrt;
-                s=e+s;
+                int *aPrt = &arr1[i][x];
+                int *bPrt = &arr2[x][j];
+                e = *aPrt * *bPrt;
+                s = e + s;
             }
-            printf("%4d\t",s);
-            s=0;
-        }  
+            printf("%4d\t", s);
+            s = 0;
+        }
         puts("");
     }
 }
